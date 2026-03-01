7b07ebbec5919d9a2ef146c5c57372fdb8b349a0
sameer kushmi <csameer795@gmail.com>
2026-03-01 14:28:48 +0545

    pending work mar 1 2:26 PM

--- components/utils/axios.js
+++ components/utils/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Create axios instance
 const api = axios.create({
-    baseURL: `${process.env.NEXT_PUBLIC_API_URL_DEV}/api`,
+    baseURL: `${process.env.NEXT_PUBLIC_API_URL_PROD}/api`,
     withCredentials: true, // IMPORTANT for cookies
 });
 
